89241fb4cd20ea64dc52bf7e20d990351f149ad4
NIKHIL JAIN <34137170+nikhiljainjain@users.noreply.github.com>
2020-03-21 14:36:23 +0530

    ready to code template

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv/config");
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let indexRouter = require('./app/routes/index');
+let usersRouter = require('./app/routes/users');
+let teamsRouter = require('./app/routes/teams');
 
-var app = express();
+let app = express();
+let mongoose = require('mongoose');
+
+let dbUrl = (process.env.NODE_ENV === 'PRODUCTION') ? process.env.MONGODB_URL : process.env.TESTDB_URL;
+
+//database connection
+mongoose.connect(dbUrl || "mongodb://localhost:27017/test", { useUnifiedTopology: true,  useNewUrlParser: true, useFindAndModify: false }, err => {
+    if (err) console.error.bind(console, 'connection error: ');
+    console.log('Connected to DataBase');
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.disable('etag');
+app.disable('x-powered-by');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/teams', teamsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,4 +52,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+console.log(`Server running on Port ${process.env.PORT || 3000}`);
 module.exports = app;
